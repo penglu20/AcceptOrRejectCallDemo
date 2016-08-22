33e0b3d5de5dfaa7a5f2ae06fda790a9adce11e8
penglu <penglu@360.cn>
2016-08-22 16:59:42 +0800

    add some introductions

--- app/src/main/java/com/pl/acceptorrejectcalldemo/incomingcall/PhoneListener.java
+++ app/src/main/java/com/pl/acceptorrejectcalldemo/incomingcall/PhoneListener.java
@@ -5,10 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class PhoneListener extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
+        Log.e("PhoneListener",action);
         if (action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
         } else {
             TelephonyManager tm = (TelephonyManager) context
@@ -18,7 +20,8 @@ public class PhoneListener extends BroadcastReceiver {
                 case TelephonyManager.CALL_STATE_RINGING:
                     incoming_number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                     try {
-                        Thread.sleep(1000);
+                        //3s后再开启activity，是为了挡在系统的接听界面之前
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
